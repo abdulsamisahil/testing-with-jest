d7732174d9b52b7db9428a0d506c75d6f99e95a0
abdulsamisahil <62578384+abdulsamisahil@users.noreply.github.com>
2022-05-05 14:47:24 +0200

    8 tests av vilka 1 medvetet fails och 7 andra passeras och ger förväntat resultat

--- tests/uppgift.test.js
+++ tests/uppgift.test.js
@@ -1,19 +1,19 @@
 const uppgift = require('../src/uppgift')
 
-//metdvetet enhetstestning 1 på fel kod som har brist vid if-satser
+//failas metdvetet då enhetstestning görs på kod som har brist vid if-satser
 test('speed i 5 ska lämna tillbaka alert alert-warning', () => {
   expect(uppgift.getBackgroundClass(5)).toBe('alert alert-warning')
 })
-//medvetet enhetstestning 1 på samma funktionen getBackgroundClass 
-test('speed i 10 ska också returnera alert alert-warning', () => {
-  expect(uppgift.getBackgroundClass(10)).toBe('alert alert-warning')
+//enhetstestning 1
+test('speed i 10 ska också returnera alert alert-success', () => {
+  expect(uppgift.getBackgroundClass(10)).toBe('alert alert-success')
 })
 
-/* // Enhetstestning 2 med  korrekta if-satser som även tar hänsyn till exkat värdena som 5
-test('speed less than 5 returns alert alert-danger', () => {
-  expect(uppgift.getClass(5)).toBe('alert alert-danger')
+ // Enhetstestning 2 på getClass med  korrekta if-satser som även tar hänsyn till exkat värdena som 5
+test('speed i 5 ska returnera alert alert-warning', () => {
+  expect(uppgift.getClass(5)).toBe('alert alert-warning')
 })
-//enhetstestning 2 med korrekta if-satser som även tar hänsyn till exkat värdena som 10
-test('speed 10 should return success', () => {
+//enhetstestning 2 på getClass med korrekta if-satser som även tar hänsyn till exkat värdena som 10
+test('speed i 10 ska lämna alert alert-success tillbaks', () => {
   expect(uppgift.getClass(10)).toBe('alert alert-success')
-}) */
+}) 
